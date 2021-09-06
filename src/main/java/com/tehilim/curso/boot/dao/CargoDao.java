85294c8e26a71b48fd9f8d8a5e77b864f485fa45
Luiz Fernando Arêas <lfernandoareas@gmail.com>
2021-09-05 21:22:16 -0300

    versão 2.01

---
+++ src/main/java/com/tehilim/curso/boot/dao/CargoDao.java
@@ -0,0 +1,21 @@
+package com.tehilim.curso.boot.dao;
+
+import java.util.List;
+
+import com.tehilim.curso.boot.domain.Cargo;
+import com.tehilim.curso.boot.util.PaginacaoUtil;
+
+public interface CargoDao {
+
+    void save(Cargo cargo );
+
+    void update(Cargo cargo);
+
+    void delete(Long id);
+
+    Cargo findById(Long id);
+
+    List<Cargo> findAll();
+    
+    PaginacaoUtil<Cargo> buscaPaginada(int pagina, String direcao);
+}
